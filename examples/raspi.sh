1d9927843626d83ce1f4f639267bc76835a430b1
Florian Hülsmann <fh@cbix.de>
2016-09-13 13:29:25 +0200

    implemented other cmds in raspi script

--- examples/raspi.sh
+++ examples/raspi.sh
@@ -7,21 +7,46 @@ export SCREEN_NAME="Raspberry Pi"
 export SCREEN_APP="pitubecast-v1"
 export OMX_OPTS="-o hdmi"
 export YTDL_OPTS="-f mp4"
+
+function omxdbus {
+    OMXPLAYER_DBUS_ADDR="/tmp/omxplayerdbus.${USER:-root}"
+    OMXPLAYER_DBUS_PID="/tmp/omxplayerdbus.${USER:-root}.pid"
+    export DBUS_SESSION_BUS_ADDRESS=`cat $OMXPLAYER_DBUS_ADDR`
+    export DBUS_SESSION_BUS_PID=`cat $OMXPLAYER_DBUS_PID`
+    dbus-send --print-reply=literal --session --dest=org.mpris.MediaPlayer2.omxplayer /org/mpris/MediaPlayer2 $* >/dev/null
+}
+
 gotubecast -s "$SCREEN_ID" -n "$SCREEN_NAME" -i "$SCREEN_APP" | while read line
 do
-	cmd="`cut -d ' ' -f1 <<< "$line"`"
-	arg="`cut -d ' ' -f2 <<< "$line"`"
-	case "$cmd" in
-		pairing_code)
-			echo "Your pairing code: $arg"
-			;;
-		remote_join)
-			cut -d ' ' -f3- <<< "$line connected"
-			;;
-		video_id)
-			YTURL="`youtube-dl -g $YTDL_OPTS https://youtube.com?v=$arg`"
-			killall omxplayer.bin
-			omxplayer $OMX_OPTS "$YTURL" &
-			;;
-	esac
+    cmd="`cut -d ' ' -f1 <<< "$line"`"
+    arg="`cut -d ' ' -f2 <<< "$line"`"
+    case "$cmd" in
+        pairing_code)
+            echo "Your pairing code: $arg"
+            ;;
+        remote_join)
+            cut -d ' ' -f3- <<< "$line connected"
+            ;;
+        video_id)
+            YTURL="`youtube-dl -g $YTDL_OPTS https://youtube.com?v=$arg`"
+            killall omxplayer.bin
+            omxplayer $OMX_OPTS "$YTURL" &
+            ;;
+        play | pause)
+            omxdbus org.mpris.MediaPlayer2.Player.PlayPause
+            ;;
+        stop)
+            omxdbus org.mpris.MediaPlayer2.Player.Stop
+            ;;
+        seek_to)
+            omxdbus org.mpris.MediaPlayer2.Player.SetPosition objpath:/not/used int64:${arg}000000
+            ;;
+        set_volume)
+            vol="1.0"
+            if [ $arg -lt 100 ]; then
+                vol="0.$arg"
+            fi
+            omxdbus org.freedesktop.DBus.Properties.Set string:"org.mpris.MediaPlayer2.Player" string:"Volume" double:$vol
+            ;;
+    esac
 done
